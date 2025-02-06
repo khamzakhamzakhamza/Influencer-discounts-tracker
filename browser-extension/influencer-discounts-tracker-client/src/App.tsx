1745bc7430134f58fac84a3370fc26bcc4b19dfa
Illia Khamza <khamzaillia@gmail.com>
2025-02-06 16:38:21 +0000

    Add client app

---
+++ browser-extension/influencer-discounts-tracker-client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
