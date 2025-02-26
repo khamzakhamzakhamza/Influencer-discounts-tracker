fd5e1ce9fc553e28a6eaa84240fc6954a4f45b5f
Illia Khamza <khamzaillia@gmail.com>
2025-02-26 00:43:03 +0000

    Add random values to mock data

--- browser-extension/influencer-discounts-tracker-client/src/services/InfluencerService.ts
+++ browser-extension/influencer-discounts-tracker-client/src/services/InfluencerService.ts
@@ -26,9 +26,33 @@ export const GetInfluencers = async (username: string): Promise<Influencer[]> =>
     {id: '1', username: '@WeeklyPlanetPodcast', title: 'The Weekly Planet', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'}
   ];
 
+  const influencers2 = [
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'},
+    {id: '1', username: '@WeeklyPlanetPodcast', title: 'Seluki', channelUrl: 'https://www.youtube.com/@WeeklyPlanetPodcast', imageUrl: 'https://yt3.googleusercontent.com/v62DJ198S7KshRsMF9Jb6z6MVPuEt3NFXebFbpLZi8KzVvuAlV476Vc_MlXlFJOfDlko3dPXE00=s160-c-k-c0x00ffffff-no-rj'}
+  ];
+
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(influencers);
+      resolve(Math.random() < 0.5 ? influencers : influencers2);
     }, 2000);
   });
   // const response = await fetch(`${API_HOST}/api/v1/influencers/${username}`);
